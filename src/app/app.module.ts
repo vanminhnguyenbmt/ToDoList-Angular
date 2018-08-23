d3e961d3685821fad17bd0dd36937a710ee4e082
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-08-23 10:19:03 +0700

    Create Project ToDoList
    Add Jquery, Bootstrap, Material Design Icons, CSS

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        TasksComponent,
+        TaskListComponent,
+        TaskItemComponent,
+        TaskFormComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
